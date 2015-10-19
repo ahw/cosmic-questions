71ad0979ec39ee4e527dded30d4dfd65c10638e4
ahw <andrewhallagan@gmail.com>
2015-10-18 18:49:38 -0700

    Mostly random CSS

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,24 +1,78 @@
-'use strict'
+'use strict';
 
 // TODO: Use Browserify and make this not a global
 let QuestionParser = window.CosmicQuestions.QuestionParser;
+console.log('hello');
 
 let APP_ID = 'cosmic-questions-' + Math.random().toString(31).substr(2,8);
 let style = document.createElement('style');
+
 style.innerHTML = `
-    #${APP_ID} {
-        font-family:Georgia,serif;
-        font-size:12px;
-        line-height:1.75;
-    }
+#${APP_ID} {
+    font-family:Monospace;
+    font-size:12px;
+    line-height:1.75;
+    position: fixed;
+    width:300px;
+    right: 0;
+    top:0;
+    bottom: 0;
+    height: ${window.innerHeight};
+    background: white;
+    padding: 10px;
+    border-left: 1px solid black;
+    overflow:scroll;
+}
 
-    #${APP_ID} ul {
-        padding-left:20px;
-    }
+#sidebar-toggle-${APP_ID} {
+    height: 100%;
+    width: 5px;
+    background: black;
+    position: absolute;
+    left:0;
+    top:0;
+    bottom:0;
+}
 
-    #${APP_ID} li {
-        padding:initial;
-    }
+#sidebar-toggle-${APP_ID}:hover {
+    height: 100%;
+    width: 5px;
+    background: yellow;
+    cursor:pointer;
+}
+
+#sidebar-toggle-${APP_ID}:after {
+    content: "\u25b6";
+    position:absolute;
+    top:50%;
+    transform:translateY(-50%);
+    left:-10px;
+    transition:500ms;
+    color:transparent;
+}
+
+#sidebar-toggle-${APP_ID}:hover:after {
+    color:black;
+    left:10px;
+}
+
+#${APP_ID} ul {
+    padding-left:20px;
+    list-style-type:circle;
+}
+
+#${APP_ID} li {
+    padding:initial;
+    font-family:inherit;
+    font-size:inherit;
+    margin-top:12px;
+    color:black;
+    line-height:1.75;;
+}
+
+#${APP_ID} li:first-of-type {
+    margin-top:0;
+}
 `;
 
 function acceptNodeFn(node) {
@@ -46,31 +100,27 @@ function run() {
 
     let div = document.createElement('div')
     div.id = APP_ID
-    div.style.position = 'fixed'
-    div.style.left = '50%'
-    div.style.bottom = '20px'
-    div.style.maxHeight = '300px'
-    div.style.overflow = 'scroll'
-    div.style.background = 'white'
-    div.style.padding = '10px'
-    div.style.boxShadow = '2px 2px 21px -4px black'
-    div.style.transform = 'translateX(-50%)'
-    div.style.border = '1px solid black'
     // div.style.width = '50%'
 
     let html = `
-    <a id="cq-close" style="float:right" href="#">Close</a>
-    <ul>
-        ${questions.map((str) => { return "<li>" + str + "</li>" }).join("")}
-    </ul>
+    <div id="sidebar-toggle-${APP_ID}"></div>
+    <a id="close-link-${APP_ID}" style="float:right" href="#">Close</a>
+    <div id="question-list-${APP_ID}">
+        <ul>
+            ${questions.map((str) => { return "<li>" + str + "</li>" }).join("")}
+        </ul>
+    </div>
     `
 
     div.innerHTML = html
 
     document.head.appendChild(style)
     document.body.appendChild(div)
-    document.getElementById('cq-close').onclick = (e) => {
+    document.getElementById(`close-link-${APP_ID}`).onclick = (e) => {
         div.remove()
+    }
+    document.getElementById(`sidebar-toggle-${APP_ID}`).onclick = (e) => {
+        div.style.width = 0;
     }
 }
 
