b5296e57916ae470de3f747238c905b6cc54a2f7
ahw <andrewhallagan@gmail.com>
2015-10-24 12:08:51 -0700

    Re-run with exponential backoff. Ignore dupes.

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -3,13 +3,30 @@
 // TODO: Use Browserify and make this not a global
 let QuestionParser = window.QuestionParser;
 let _ = window._;
+let questionIds = {};
 
 function acceptNodeFn(node) {
     return /\?/.test(node.textContent)
 }
 
+function isNewQuestion(question) {
+    // let hash = Sha1.hash(window.location.host + question.text);
+    let key = window.location.host + question.text;
+    if (questionIds[key]) {
+        return false;
+    } else {
+        console.log('Ignoring duplicate question:', key);
+        questionIds[key] = question;
+        return true;
+    }
+}
 
-function run() {
+
+function run(interval) {
+    if (typeof interval === 'undefined') {
+        interval = 1000;
+    }
+
     let walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, {acceptNode: acceptNodeFn})
     let node = walker.nextNode()
     let allQuestions = [];
@@ -23,11 +40,15 @@ function run() {
     }
 
     allQuestions.map((question, index) => {
-        console.log(index + '. ' + question.id + ': ' + question.text);
-        question.mutation();
-        question.testVisibilty();
+        if (isNewQuestion(question)) {
+            console.log(index + '. ' + question.id + ': ' + question.text);
+            question.mutation();
+            question.testVisibilty();
+        }
     });
 
+    console.log('Cosmic questions found ' + allQuestions.length + ' questions.');
+    setTimeout(run.bind(null, interval*1.8), interval * 1.8);
 }
 
 window.addEventListener('load', () => {
