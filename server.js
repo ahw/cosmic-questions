1fcb021b2f75974fbd72f64cad43df56a7ae6052
ahw <andrewhallagan@gmail.com>
2015-10-24 17:54:24 -0700

    Log questions to questions.log
    Each line is a JSON object.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 app.use(bodyParser.json()); // for parsing application/json
 
@@ -13,6 +14,7 @@ app.post('/questions', function(request, response) {
 
     if (request.body.questionList) {
         request.body.questionList.map(function(question) {
+            fs.appendFileSync('questions.log', JSON.stringify(question) + "\n");
             console.log(request.body.host + ': ' + question.text);
             // console.log('     > ' + request.body.location.href);
         });
